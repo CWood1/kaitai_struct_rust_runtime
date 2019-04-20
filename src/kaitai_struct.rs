d7db8856b0c877bf6ecc10fb9387178d6ee7aead
Connor Wood <connorwood71@gmail.com>
2019-04-20 22:09:13 +0100

    Stream should be moved, not borrowed

--- src/kaitai_struct.rs
+++ src/kaitai_struct.rs
@@ -4,18 +4,18 @@ use kaitai_stream::KaitaiStream;
 pub trait KaitaiStruct<'a> {
     fn from_file(path: &str) -> std::io::Result<Self> where Self : Sized {
         let mut f = std::fs::File::open(path)?;
-        Self::new(&mut f, None, None)
+        Self::new(f, None, None)
     }
     
     fn from_bytes(bytes: Vec<u8>) -> std::io::Result<Self> where Self : Sized {
         let mut b = std::io::Cursor::new(bytes);
-        Self::new(&mut b, None, None)
+        Self::new(b, None, None)
     }
     
-    fn new<S: KaitaiStream>(stream: &'a mut S,
-                            parent: Option<&'a KaitaiStruct<'a>>,
-                            root: Option<&'a KaitaiStruct<'a>>)
-                            -> std::io::Result<Self>
+    fn new(stream: KaitaiStream,
+           parent: Option<&'a KaitaiStruct<'a>>,
+           root: Option<&'a KaitaiStruct<'a>>)
+           -> std::io::Result<Self>
         where Self : Sized;
     
     fn read(&mut self)
